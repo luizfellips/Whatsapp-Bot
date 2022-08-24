0ca81fcbab16b1a30ac2dbf0ffa2135bfc1af94b
Luiz Fellipe <110192027+luizfellips@users.noreply.github.com>
2022-08-24 08:13:21 -0300

    Removido pacotes inutilizados

--- interface.py
+++ interface.py
@@ -1,15 +1,12 @@
-from cmath import isnan, nan
 from tkinter import *
 from tkinter import ttk
 from tkinter.filedialog import askopenfilename
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from time import sleep
 import pandas as pd
-
 
 
 class MainApplication:
